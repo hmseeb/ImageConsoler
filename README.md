0c7372f3a3db831b6c14004de979a8ba3cbd2955
Haseeb Azhar <haseebazhar6389@gmail.com>
2023-01-11 14:47:29 +0500

    v1.9

---
+++ README.md
@@ -0,0 +1,57 @@
+<p align="center">
+<img src="https://capsule-render.vercel.app/api?type=waving&color=gradient&height=200&section=header&text=ImageConsoler&fontSize=80&fontAlignY=35&animation=twinkling&fontColor=gradient"/> </a> 
+</p>
+
+<img align="left" width=100 height=100 src="https://i.imgur.com/STghnjI.png"  />
+
+Print your image in console! <br>*Now with all the colors you want*
+```
+./ConsoleImager {link} {[Optional]height} {[Optional]width}
+```
+
+# ⚙️ `Usage`
+
+## 🪟 `Windows`
+> Command syntax:
+```cs
+./ConsoleImager.exe {link} {[Optional]height} {[Optional]width}
+```
+
+> Examples: 
+```cs
+./ConsoleImager.exe "https://upload.wikimedia.org/wikipedia/en/e/ed/Nyan_cat_250px_frame.PNG"
+```
+
+```cs
+./ConsoleImager.exe "https://upload.wikimedia.org/wikipedia/en/e/ed/Nyan_cat_250px_frame.PNG" 100 
+```
+
+```cs
+./ConsoleImager.exe "https://upload.wikimedia.org/wikipedia/en/e/ed/Nyan_cat_250px_frame.PNG" 100 50 
+```
+
+## 🐧 `Linux`
+> Configuration
+Allow user to use the program
+```cs
+chown $USER:$USER ConsoleImager
+chmod 755 ./ConsoleImager
+```
+
+> Command syntax: 
+```cs
+./ConsoleImager {link} {[Optional]height} {[Optional]width}
+```
+
+## 🏗️ `Build it yourself`
+If release doesn't contain your OS system, you can build one by yourself!
+
+Example of publish command in `ConsoleImager/src` folder
+```cs
+dotnet publish -c release -r linux-x64 -p:PublishSingleFile=true -o ./linux-x64 
+```
+Publish command docs: https://learn.microsoft.com/en-us/dotnet/core/tools/dotnet-publish 
+
+RID publish docs: https://learn.microsoft.com/en-us/dotnet/core/rid-catalog
+
+
---
+++ src/Program.cs
@@ -0,0 +1,56 @@
+﻿using SkiaSharp;
+
+HttpClient client = new();
+
+var imageUrl = args.Length != 0 ? args[0] : "https://preview.redd.it/653ztm9yjtg31.jpg?auto=webp&s=da7aeb94db7f956bea0f2a4b1543af4714cbadc4";
+var imageBytes = await client.GetByteArrayAsync(imageUrl);
+var bitmap = SKBitmap.Decode(imageBytes);
+
+var ratio = (double)bitmap.Width / bitmap.Height;
+int width = !(args.Length <= 1) ? Convert.ToInt32(args[1]) : 100;
+int height = !(args.Length <= 2) ? Convert.ToInt32(args[2]) : (int)(width / ratio);
+
+SKBitmap resizedImage = new(width, height);
+bitmap.ScalePixels(resizedImage, GetFilterQuality(width));
+
+ParallelOptions options = new()
+{
+    MaxDegreeOfParallelism = Environment.ProcessorCount
+};
+
+string[,] outputArr = new string[resizedImage.Height, resizedImage.Width + 1];
+Parallel.For(0, resizedImage.Height, options, (heightIndex) =>
+{
+    for (int widthIndex = 0; widthIndex < resizedImage.Width; widthIndex++)
+    {
+        outputArr[heightIndex, widthIndex] = GetPrinter(resizedImage.GetPixel(widthIndex, heightIndex));
+    }
+
+    outputArr[heightIndex, resizedImage.Width] = Environment.NewLine;
+});
+
+string output = "";
+for (int h = 0; h < outputArr.GetLength(0); h++)
+{
+    output = "";
+    for (int w = 0; w < outputArr.GetLength(1); w++)
+    {
+        output += outputArr[h, w];
+    }
+
+    Console.Write(output);
+}
+
+static string GetPrinter(SKColor color)
+{
+    var result = $"\u001b[38;2;{color.Red};{color.Green};{color.Blue}m█";
+    return result;
+}
+
+static SKFilterQuality GetFilterQuality(int width)
+{
+    if (width <= 100) return SKFilterQuality.Low;
+    else if (width <= 500) return SKFilterQuality.Medium;
+    else if (width <= 1500) return SKFilterQuality.High;
+    else return SKFilterQuality.Low;
+}
